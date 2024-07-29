130a09c1a873259e6743ee10a5d8ca88c60f26ba
Subham <subhampaul456@gmail.com>
2024-07-29 13:20:43 +0530

    minor changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import java.util.Scanner;
-// import Scanner class which is in util folder inside the java folder.
-// Util stands for utility and contains utility classes.
-// Scanner is a predefined class for taking inputs from user.
-
-public class HelloWorld{
-    c
-        // System.out.println("Hello Subham!");
-
-        // System.out.println();
-        // Scanner subham = new Scanner(System.in);
-        // System.out.print("enter a number here ");
-        // int paul = subham.nextInt();
-        // System.out.println("Print the number is " + paul);
-        String s;
-        int freq [] = new int[26];
-        for (int i = 0; i < freq.length(); i++) {
-            System.out.println(freq[i]);
-        }
-    }
-=======
 import java.util.Scanner;
 // import Scanner class which is in util folder inside the java folder.
 // Util stands for utility and contains utility classes.
@@ -35,5 +13,4 @@ public class HelloWorld{
         int paul = subham.nextInt();
         System.out.println("Print the number is " + paul);
     }
->>>>>>> 52103293dc0c9a353aaf2603609107cc1527a8f9
 }
